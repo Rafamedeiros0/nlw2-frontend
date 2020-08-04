9cf3582aec8daf15044b64897bf9b03fe31c687d
RafaMedeiros0 <rafamedeiros0@gmail.com>
2020-08-03 21:06:48 -0300

    create form in TeacherList and config css for PC

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -10,7 +10,7 @@ interface PageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = () => {
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
         <div className="top-bar-container">
@@ -21,7 +21,9 @@ const PageHeader: React.FC<PageHeaderProps> = () => {
         </div>
 
         <div className="header-content">
-          <strong>Estes são os proffys disponíveis.</strong>
+          <strong>{props.title}</strong>
+          
+          {props.children}
         </div>
       </header>
   )
