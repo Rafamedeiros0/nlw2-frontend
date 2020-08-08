ab8be0706611ab0ef4f1933b9c09bdc9cf08faff
RafaMedeiros0 <rafamedeiros0@gmail.com>
2020-08-08 17:34:23 -0300

    final connect api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,56 @@
 import React from 'react'
 
+import api from '../../services/api'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ( {teacher} ) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
+
   return (
     <article className="teacher-item">
     <header>
-      <img src="https://avatars0.githubusercontent.com/u/32855003?s=460&u=1b972d170f3adf734016fdf7be4664479c92711c&v=4" alt="Rafael Medeiros"/>
+      <img src={teacher.avatar} alt={teacher.name}/>
       <div>
-        <strong>Rafael Medeiros</strong>
-        <span>Química</span>
+      <strong>{teacher.name}</strong>
+      <span>{teacher.subject}</span>
       </div>
     </header>
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      <br/>            
-      <br/>            
-      Ipsa est corrupti quasi quas quaerat modi sed aspernatur illo velit? Doloremque voluptatibus reiciendis corrupti nulla eum. Ad rerum voluptatibus sint ipsam.
-    </p>
+    <p>{teacher.bio}</p>
     <footer>
       <p>
         Preço/hora
-        <strong>R$ 80,00</strong>
+        <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a 
+        target="_blank"
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.whatsapp}`}
+      >
         <img src={whatsappIcon} alt="Whatsapp" />
         Entrar em contato
-      </button>
+      </a>
     </footer>
     </article>
   )
